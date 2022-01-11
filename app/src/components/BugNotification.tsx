6f68fc3037ae0cd8b2ab254f4729e130a38817d5
Timo Wilhelm <mail@timowilhelm.com>
2022-01-11 19:11:21 +0100

    add data-nosnippet to notification header

--- app/src/components/BugNotification.tsx
+++ app/src/components/BugNotification.tsx
@@ -8,34 +8,34 @@ export function BugNotification() {
   const [visible, setVisible] = useState(isChromium);
 
   return (
-    <div
-      className={classNames(
-        'flex text-white text-center font-medium bg-red-900',
-        { hidden: !visible },
-      )}
-    >
-      <div className="flex-1">
-        <span>
-          Please note that chromium based browsers currently have an{' '}
-          <a
-            href="https://bugs.chromium.org/p/chromium/issues/detail?id=612542"
-            className="text-blue-400 underline"
-          >
-            issue
-          </a>{' '}
-          that severely affects performance. For best results, please use
-          Firefox.
-        </span>
-      </div>
-      <button
-        type="button"
-        onClick={() => {
-          setVisible(false);
-        }}
-        className="px-2"
+    <aside className={classNames({ hidden: !visible })}>
+      <div
+        data-nosnippet
+        className="flex text-white text-center font-medium bg-red-900"
       >
-        <XIcon className="h-5 w-5" />
-      </button>
-    </div>
+        <div className="flex-1">
+          <span>
+            Please note that chromium based browsers currently have an{' '}
+            <a
+              href="https://bugs.chromium.org/p/chromium/issues/detail?id=612542"
+              className="text-blue-400 underline"
+            >
+              issue
+            </a>{' '}
+            that severely affects performance. For best results, please use
+            Firefox.
+          </span>
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            setVisible(false);
+          }}
+          className="px-2"
+        >
+          <XIcon className="h-5 w-5" />
+        </button>
+      </div>
+    </aside>
   );
 }
