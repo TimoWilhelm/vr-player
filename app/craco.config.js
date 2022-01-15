efa2bb2142913ebd23316131c0560ff11b134985
Timo Wilhelm <mail@timowilhelm.com>
2022-01-15 17:07:47 +0100

    use CANONICAL_URL

--- app/craco.config.js
+++ app/craco.config.js
@@ -4,7 +4,6 @@ process.env.BROWSER = 'none';
 
 if (process.env.CI === '1') {
   process.env.GENERATE_SOURCEMAP = 'false';
-  process.env.PUBLIC_URL = `https://${process.env.REACT_APP_VERCEL_URL}`;
 }
 
 // TODO: https://github.com/pmndrs/jotai/issues/965
