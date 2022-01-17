e0738c299d3b7d3cc5c9907a755899226bb4833b
Timo Wilhelm <mail@timowilhelm.com>
2022-01-17 11:12:59 +0100

    update jotai

--- app/craco.config.js
+++ app/craco.config.js
@@ -5,14 +5,3 @@ process.env.BROWSER = 'none';
 if (process.env.CI === '1') {
   process.env.GENERATE_SOURCEMAP = 'false';
 }
-
-// TODO: https://github.com/pmndrs/jotai/issues/965
-module.exports = {
-  webpack: {
-    plugins: [
-      new DefinePlugin({
-        process: JSON.stringify({}),
-      }),
-    ],
-  },
-};
